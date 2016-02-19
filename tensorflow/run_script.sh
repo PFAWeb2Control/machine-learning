2c594880e1570d7fcd05116028cfca537fc80215
Antoine <maschmalow@gmail.com>
2016-02-19 03:29:48 +0100

    keep docker history

--- tensorflow/run_script.sh
+++ tensorflow/run_script.sh
@@ -2,4 +2,4 @@
 
 SCRIPTS_LOCATION=~/machine-learning/tensorflow
 
-docker run --rm -it -v $SCRIPTS_LOCATION:/root b.gcr.io/tensorflow/tensorflow python $1 > output_$1.txt
+docker run -it -v $SCRIPTS_LOCATION:/root b.gcr.io/tensorflow/tensorflow python $1 > output_$1.txt
