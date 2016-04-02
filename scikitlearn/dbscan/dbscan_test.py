4b6c80c6eb0cf81d84e1a33b678e7842d1a907f0
irifki <rifkiimane@gmail.com>
2016-04-02 14:36:19 +0200

    Update dbscan_test.py

--- scikitlearn/dbscan/dbscan_test.py
+++ scikitlearn/dbscan/dbscan_test.py
@@ -1,21 +1,19 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 from sklearn.cluster import DBSCAN
 from sklearn import metrics
 from sklearn.datasets.samples_generator import make_blobs
 from sklearn.preprocessing import StandardScaler
 
 
-##############################################################################
 # Generate sample data
 centers = [[1, 1, -1], [-1, -1, -1], [1, -1, 1]]
-X, labels_true = make_blobs(n_samples=750, centers=centers, cluster_std=0.4,
+X, labels_true = make_blobs(n_samples=1000, centers=centers, cluster_std=0.4,
                             random_state=0)
 print(X)
 
 X = StandardScaler().fit_transform(X)
 
-##############################################################################
 # Compute DBSCAN
 db = DBSCAN(eps=0.3, min_samples=10).fit(X)
 core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
@@ -26,20 +24,8 @@ labels = db.labels_
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 
 print('Estimated number of clusters: %d' % n_clusters_)
-print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
-print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
-print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
-print("Adjusted Rand Index: %0.3f"
-      % metrics.adjusted_rand_score(labels_true, labels))
-print("Adjusted Mutual Information: %0.3f"
-      % metrics.adjusted_mutual_info_score(labels_true, labels))
-print("Silhouette Coefficient: %0.3f"
-      % metrics.silhouette_score(X, labels))
 
-##############################################################################
-# Plot result
-import matplotlib.pyplot as plt
-
+# Plot the results
 # Black removed and is used for noise instead.
 unique_labels = set(labels)
 colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
