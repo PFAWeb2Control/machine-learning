5369a9c8cf7244d52ec1611cfb8f2abcd9ebb5e0
ClementC <ccook@enseirb-matmeca.fr>
2016-04-03 00:03:11 +0200

    Correction test_sequential_birch.py

--- scikitlearn/birch/test_sequential_birch.py
+++ scikitlearn/birch/test_sequential_birch.py
@@ -9,14 +9,27 @@ n_samples = 50
 centers = [[0, 1], [4, -2], [-2, 2], [0, -1]]
 X, _ = make_blobs(n_samples=n_samples, centers=centers, cluster_std=0.2)
 
-# Prints the points generated
-plt.plot([X[a][0] for a in range(n_samples)], [X[b][1] for b in range(n_samples)], '.')
-plt.axis([-4,12,-4,12])
-plt.show()
-
 # Creates the Birch classificator and gives it the vectors
 brc = Birch(branching_factor=50, n_clusters=None, threshold=0.8, compute_labels=True)
 brc.fit(X)
+
+labels = brc.labels_
+cluster_centers = brc.subcluster_centers_
+labels_unique = np.unique(labels)
+n_clusters_ = len(labels_unique)
+
+# Prints the points generated
+plt.figure(1)
+plt.clf()
+
+colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
+for k, col in zip(range(n_clusters_), colors):
+    my_members = labels == k
+    cluster_center = cluster_centers[k]
+    plt.plot(X[my_members, 0], X[my_members, 1], col + '.')
+    plt.axis([-4,12,-4,12])
+plt.title('Estimated number of clusters: %d' % n_clusters_)
+plt.show()
 
 # Generates new vectors to clusters, some close to an already existant cluster ([0,1]), the others far from any clusters
 n_samples_2 = 200
@@ -54,7 +67,6 @@ for k, col in zip(range(n_clusters_), colors):
     my_members = labels == k
     cluster_center = cluster_centers[k]
     plt.plot(X_tot[my_members, 0], X_tot[my_members, 1], col + '.')
-    plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col, markeredgecolor='k', markersize=14)
     plt.axis([-4,12,-4,12])
 plt.title('Estimated number of clusters: %d' % n_clusters_)
 plt.show()
