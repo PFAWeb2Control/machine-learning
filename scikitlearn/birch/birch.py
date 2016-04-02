8547040aaefeb194e40d9ba50dc461167b871205
irifki <rifkiimane@gmail.com>
2016-04-02 12:54:33 +0200

    Update birch.py

--- scikitlearn/birch/birch.py
+++ scikitlearn/birch/birch.py
@@ -4,7 +4,22 @@ from time import time
 import numpy as np
 
 from sklearn.cluster import Birch
-    
+"""
+A function that runs the birch clustering algorithm.
+
+Parameters:
+-X is our input data (an array)
+-threshold is the radius of the subcluster obtained by
+merging a new sample and the closest subcluster should
+be lesser than the threshold. Default value is 1.7.
+-clustering is number of cluster after the final clustering
+which treats the subclusters from the leaves as new sample.
+By default, this final clustering step is not performed and
+the subclusters are returned as they are.
+
+This function returns the labels of the clusters, their centroids
+and their number.
+""" 
 def birch_algo(X, threshold=1.7, clustering=None):
         birch = Birch(threshold=threshold, n_clusters=clustering)
         birch.fit(X)
@@ -13,12 +28,6 @@ def birch_algo(X, threshold=1.7, clustering=None):
         labels_unique = np.unique(labels)
         n_clusters = labels_unique.size
         print(" The number of clusters is : %d" % n_clusters)
-
-     #    print("Centre(s):")
-     #    for i in range(n_clusters):
-     #    	print i,
-     #    	print centroids[i]
-
         return labels, centroids, n_clusters     
 
 
